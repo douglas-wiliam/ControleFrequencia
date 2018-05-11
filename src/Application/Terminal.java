cb2b5c41cc76734d5616c38b388ebb503ba4d11d
Douglas Wiliam <dougl.wil@gmail.com>
2018-05-11 20:31:28 -0300

    Metodo getInputTecladoRemovido

--- src/Application/Terminal.java
+++ src/Application/Terminal.java
@@ -1,7 +1,6 @@
 package Application;
 
 import Façade.Empresa;
-import java.util.Scanner;
 
 /*
  * Indicar o tipo de licença aqui.
@@ -11,18 +10,6 @@ import java.util.Scanner;
  * @author douglas
  */
 public class Terminal {
-
-    private static String[] getInputTeclado() {
-        String input;
-        String[] tokens;
-        Scanner scanner = new Scanner(System.in);
-        input = scanner.nextLine();
-
-        tokens = input.split(" ");
-
-        return tokens;
-
-    }
 
     public static void main(String[] args) {
 
