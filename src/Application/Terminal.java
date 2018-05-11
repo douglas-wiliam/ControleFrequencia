7f443ac2d1afb4733263863d9d52aabd94dcb5ac
Douglas Wiliam <dougl.wil@gmail.com>
2018-05-11 20:30:18 -0300

    Terminal transformador em caller da GUI

--- src/Application/Terminal.java
+++ src/Application/Terminal.java
@@ -1,13 +1,6 @@
 package Application;
 
-import Command.ComandoExibeFrequenciaFuncionarioPorPeriodo;
-import Command.ComandoInicializarDados;
-import Command.Comando;
-import Command.ComandoAlteraNomeEmpresa;
-import Command.Invocador;
 import Façade.Empresa;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 /*
@@ -32,45 +25,11 @@ public class Terminal {
     }
 
     public static void main(String[] args) {
-        String[] input;
-        String output;
-        Map<String, Comando> mapeamento = new HashMap<>();
-
-        Comando comando = new ComandoInicializarDados();
-        ComandoExibeFrequenciaFuncionarioPorPeriodo ceffpp = null;
-        ComandoAlteraNomeEmpresa cane = null;
-
-        System.out.print("@Empresa: ");
-        input = getInputTeclado();
 
         Empresa.getInstance();
 
-        Invocador invocador = new Invocador();
-        invocador.executarComando(comando);
-
-        while (!"sai".equals(input[0])) {
-            
-            if (input.length == 2) {    
-                cane = new ComandoAlteraNomeEmpresa(input[1]);  // Ainda não suporta nomes com espaço
-                mapeamento.put("nome", cane);
-            }
-
-            if (input.length == 4) {
-                ceffpp = new ComandoExibeFrequenciaFuncionarioPorPeriodo(input[1], input[2], input[3]);
-                mapeamento.put("freq", ceffpp);
-            }
-
-            // Não trata comandos inexistentes
-            comando = mapeamento.get(input[0]);
-
-            output = invocador.executarComando(comando);
-
-            System.out.println(output);
-            System.out.print("@Empresa: ");
-            input = getInputTeclado();
-        }
-
-        System.out.println("Usuário saiu do sistema.");
+        GUI gui = new GUI();
+        gui.setVisible(true);
     }
 
 }
