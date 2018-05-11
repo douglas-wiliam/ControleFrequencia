62ada2a96c29815370e027ef05103b3664894b5b
Douglas Wiliam <dougl.wil@gmail.com>
2018-05-10 22:38:30 -0300

    Objetos Comando movidos para dentro do loop While em mais() Adicionada chamada ao metodo Empresa.getInstance() para instanciacao do singleton

--- src/Application/Terminal.java
+++ src/Application/Terminal.java
@@ -5,6 +5,7 @@ import Command.ComandoInicializarDados;
 import Command.Comando;
 import Command.ComandoAlteraNomeEmpresa;
 import Command.Invocador;
+import Façade.Empresa;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -34,24 +35,30 @@ public class Terminal {
         String[] input;
         String output;
         Map<String, Comando> mapeamento = new HashMap<>();
+
         Comando comando = new ComandoInicializarDados();
-        Invocador invocador = new Invocador();
+        ComandoExibeFrequenciaFuncionarioPorPeriodo ceffpp = null;
+        ComandoAlteraNomeEmpresa cane = null;
+
         System.out.print("@Empresa: ");
         input = getInputTeclado();
 
-        ComandoExibeFrequenciaFuncionarioPorPeriodo ceffpp = new ComandoExibeFrequenciaFuncionarioPorPeriodo(input[1], input[2], input[3]);
-        mapeamento.put("freq", ceffpp);
+        Empresa.getInstance();
 
-        ComandoAlteraNomeEmpresa cane = new ComandoAlteraNomeEmpresa(input[1]);
-        mapeamento.put("nome", cane);
-        
+        Invocador invocador = new Invocador();
         invocador.executarComando(comando);
 
         while (!"sai".equals(input[0])) {
-            
+
+            ceffpp = new ComandoExibeFrequenciaFuncionarioPorPeriodo(input[1], input[2], input[3]);
+            mapeamento.put("freq", ceffpp);
+
+            cane = new ComandoAlteraNomeEmpresa(input[1]);
+            mapeamento.put("nome", cane);
+
             // Não trata comandos inexistentes
             comando = mapeamento.get(input[0]);
-            
+
             output = invocador.executarComando(comando);
 
             System.out.println(output);
